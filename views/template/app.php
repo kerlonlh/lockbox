8bb8065e7f5f5cfad1e67b153f03642331b1b739
Kerlon Hinterholz <kerlon.l.h@gmail.com>
2025-06-17 22:36:09 -0300

    Ajuste no tema do teamplate

--- views/template/app.php
+++ views/template/app.php
@@ -1,15 +1,17 @@
 <!DOCTYPE html>
-<html lang="pt-BR" data-theme="dracula">
+<html lang="en">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>LockBox</title>
-    <link href="https://cdn.jsdelivr.net/npm/daisyui@5" rel="stylesheet" type="text/css"/>
+    <link href="https://cdn.jsdelivr.net/npm/daisyui@4.12.10/dist/full.min.css" rel="stylesheet" type="text/css"/>
     <script src="https://cdn.tailwindcss.com"></script>
+    <link href="https://cdn.jsdelivr.net/npm/daisyui@5" rel="stylesheet" type="text/css" />
+    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
 </head>
 
-<body class="bg-stone-950 text-stone-200">
+<body>
     <?php require "../views/{$view}.view.php"; ?>
 </body>
 
