68e6f6050b33f481162512bc0f829b56a4d1f36c
Kerlon Hinterholz <kerlon.l.h@gmail.com>
2025-06-28 15:48:13 -0300

    Melhorando nosso sistema de rotas

---
+++ App/Controllers/LoginController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Controllers;
+
+class LoginController{
+    public function index(){
+
+    }
+
+    public function login(){
+
+    }
+
+}
--- routes.php
+++ routes.php
@@ -1,5 +1,19 @@
 <?php 
 
+
+    use Core\Route;
+    use App\Controllers\IndexController;
+    use App\Controllers\LoginController;
+
+    (new Route())
+
+    ->get('/', IndexController::class)
+    ->get('/login', [LoginController::class, 'index'])
+    ->post('/login', [LoginController::class, 'login'])
+    ->run();
+
+    die();
+
     $controller = str_replace ('/', '', parse_url($_SERVER['REQUEST_URI'])['path']);
     if (! $controller) $controller = 'index';
 
