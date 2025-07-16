a3f41fe6da7e7b5ec9c96dbe0a8c1a7d08bbfce6
Kerlon Hinterholz <kerlon.l.h@gmail.com>
2025-07-15 22:14:07 -0300

    Criando uma nota

--- views/notas/criar.view.php
+++ views/notas/criar.view.php
@@ -1,3 +1,6 @@
+
+<?php $validacoes = flash()->get('validacoes'); ?>
+
 <div class="bg-base-300 rounded-l-box w-56">
     <div class="bg-base-200 p-4">
         + Nova nota
@@ -7,18 +10,25 @@
 
 <div class="bg-base-200 rounded-r-box w-full p-10">
     <form action="/notas/criar" method="POST" class=" flex flex-col space-y-6">
+        
         <label for="" class="form-control w-full">
             <div class="label">
                 <span class="label-text">Título</span>
             </div>
-            <input type="text" placeholder="type here" class="input input-bordered w-full">
+            <input type="text" name="titulo" class="input input-bordered w-full">
+            <?php if (isset($validacoes['titulo'])) : ?>
+                <div class="label text-xs text-error"><?= $validacoes['titulo'][0] ?></div>
+            <?php endif; ?>
         </label>
 
         <label for="" class="form-control">
             <div class="label">
                 <span class="label-text">Sua nota</span>
             </div>
-            <textarea placeholder="Bio" class="textarea textarea-bordered h-24 w-full"></textarea>
+            <textarea name="nota" class="textarea textarea-bordered h-24 w-full"></textarea>
+            <?php if (isset($validacoes['nota'])) : ?>
+                <div class="label text-xs text-error"><?= $validacoes['nota'][0] ?></div>
+            <?php endif; ?>
         </label>
 
         <div class="flex justify-end items-center">
