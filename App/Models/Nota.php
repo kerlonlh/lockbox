fbcb5590fbd1ac60d6dcbad043cb661110c87f28
Kerlon Hinterholz <kerlon.l.h@gmail.com>
2025-07-17 21:26:01 -0300

    Sistema de pesquisa de notas

--- App/Models/Nota.php
+++ App/Models/Nota.php
@@ -13,16 +13,16 @@ class Nota{
     public $data_atualizacao;
 
 
-    public static function all() {
+    public static function all($pesquisar = null) {
         
         $db = new Database(config('database'));
 
         return $db->query(
-            query: "SELECT * FROM notas WHERE usuario_id = :usuario_id",
+            query: "SELECT * FROM notas WHERE usuario_id = :usuario_id " . (
+                $pesquisar ? "AND titulo like :pesquisar" : null
+            ),
             class: self::class,
-            params: [
-                'usuario_id' => auth()->id
-            ]
+            params: array_merge(['usuario_id' => auth()->id], $pesquisar ? ['pesquisar' => "%$pesquisar%"] : [])
         )->fetchAll();
     }
 }
